0b145fb0d077f585ee450c6715bd17f5ce99eb5b
unknown <Z.AITAADDI@LAP-SMARAR.copag.local>
2018-03-26 14:45:18 +0000

    Change database name

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ UPLOAD_FOLDER = os.path.join(app.instance_path, "uploads")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # SQLAlchemy config
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:copag@localhost/audit_v1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:copag@localhost/audit_v2'
 db = SQLAlchemy(app)
 
 
