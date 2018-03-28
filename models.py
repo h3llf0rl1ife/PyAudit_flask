11529fdd4e00512ecfefb5f32984a14dcd9e0ec0
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-03-28 09:09:40 +0000

    Fix table names in foreign keys

--- models.py
+++ models.py
@@ -30,9 +30,9 @@ class User(db.Model):
 class Evaluation(db.Model):
     __tablename__ = "T_EVALUATION"
     EvaluationID = db.Column(db.Integer, primary_key=True)
-    UserID = db.Column(db.String, db.ForeignKey('user.UserID'), nullable=False)
-    LocationID = db.Column(db.Integer, db.ForeignKey('location.LocationID'), nullable=False)
-    CategoryID = db.Column(db.Integer, db.ForeignKey('category.CategoryID'), nullable=False)
+    UserID = db.Column(db.String, db.ForeignKey('T_USER.UserID'), nullable=False)
+    LocationID = db.Column(db.Integer, db.ForeignKey('T_LOCATION.LocationID'), nullable=False)
+    CategoryID = db.Column(db.Integer, db.ForeignKey('T_CATEGORY.CategoryID'), nullable=False)
     Date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     Validation = db.Column(db.Integer, nullable=False)
     Comment = db.Column(db.Text, nullable=True) # Remarque
@@ -48,8 +48,8 @@ class Evaluation(db.Model):
 class EvaluationEdit(db.Model):
     __tablename__ = "T_EVALUATION_EDIT"
     EvaluationEditID = db.Column(db.Integer, primary_key=True)
-    EvaluationID = db.Column(db.Integer, db.ForeignKey('evaluation.EvaluationID'), nullable=False)
-    UserID = db.Column(db.String, db.ForeignKey('user.UserID'), nullable=False)
+    EvaluationID = db.Column(db.Integer, db.ForeignKey('T_EVALUATION.EvaluationID'), nullable=False)
+    UserID = db.Column(db.String, db.ForeignKey('T_USER.UserID'), nullable=False)
     Date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     evaluation_r = db.relationship('Evaluation', backref=db.backref('evaluation_edits', lazy=True))
@@ -62,7 +62,7 @@ class EvaluationEdit(db.Model):
 class Category(db.Model):
     __tablename__ = "T_CATEGORY"
     CategoryID = db.Column(db.Integer, primary_key=True)
-    CriteriaID = db.Column(db.String, db.ForeignKey('criteria.CriteriaID'), nullable=False)
+    CriteriaID = db.Column(db.String, db.ForeignKey('T_CRITERIA.CriteriaID'), nullable=False)
     Description = db.Column(db.Text, nullable=False)
 
     criteria_r = db.relationship('Criteria', backref=db.backref('criterias', lazy=True))
@@ -82,8 +82,8 @@ class Criteria(db.Model):
 
 class Feedback(db.Model):
     __tablename__ = "T_EVALUATION_FEEDBACK"
-    EvaluationID = db.Column(db.Integer, db.ForeignKey('evaluation.EvaluationID'), primary_key=True)
-    UserID = db.Column(db.String, db.ForeignKey('user.UserID'), nullable=False)
+    EvaluationID = db.Column(db.Integer, db.ForeignKey('T_EVALUATION.EvaluationID'), primary_key=True)
+    UserID = db.Column(db.String, db.ForeignKey('T_USER.UserID'), nullable=False)
     Feedback = db.Column(db.Text, nullable=False)
 
     evaluation_r = db.relationship('Evaluation', backref=db.backref('eval_feedback', lazy=True))
@@ -106,7 +106,7 @@ class Attachment(db.Model):
     __tablename__ = "T_ATTACHMENT"
     AttachmentID = db.Column(db.Integer, primary_key=True)
     EvaluationID = db.Column(db.Integer, db.ForeignKey('evaluation.EvaluationID'), nullable=False)
-    AttachmentTypeID = db.Column(db.Integer, db.ForeignKey('attachment_type.AttachmentTypeID'), nullable=False)
+    AttachmentTypeID = db.Column(db.Integer, db.ForeignKey('T_ATTACHMENT_TYPE.AttachmentTypeID'), nullable=False)
     Path = db.Column(db.String, unique=True, nullable=False)
 
     attachment_type_r = db.relationship('AttachmentType', backref=db.backref('attachment_types', lazy=True))
@@ -128,8 +128,8 @@ class Site(db.Model):
 class Zone(db.Model):
     __tablename__ = "T_ZONE"
     ZoneID = db.Column(db.Integer, primary_key=True)
-    UserID = db.Column(db.String, db.ForeignKey('user.UserID'), nullable=False)
-    SiteID = db.Column(db.Integer, db.ForeignKey('site.SiteID'), nullable=False)
+    UserID = db.Column(db.String, db.ForeignKey('T_USER.UserID'), nullable=False)
+    SiteID = db.Column(db.Integer, db.ForeignKey('T_SITE.SiteID'), nullable=False)
     ZoneName = db.Column(db.String, nullable=False)
 
     user_r = db.relationship('User', backref=db.backref('user_zone', lazy=True))
@@ -143,7 +143,7 @@ class Unit(db.Model): # Unité
     __tablename__ = "T_UNIT"
     UnitID = db.Column(db.Integer, primary_key=True)
     UnitName = db.Column(db.String, nullable=False)
-    ZoneID = db.Column(db.Integer, db.ForeignKey('zone.ZoneID'), nullable=False)
+    ZoneID = db.Column(db.Integer, db.ForeignKey('T_ZONE.ZoneID'), nullable=False)
 
     zone_r = db.relationship('Zone', backref=db.backref('zone_unit', lazy=True))
 
@@ -155,8 +155,8 @@ class Location(db.Model): # Localisation
     __tablename__ = "T_LOCATION"
     LocationID = db.Column(db.Integer, primary_key=True)
     LocationName = db.Column(db.String, nullable=False)
-    UnitID = db.Column(db.Integer, db.ForeignKey('unit.UnitID'), nullable=False)
-    LocationTypeID = db.Column(db.Integer, db.ForeignKey('location_type.LocationTypeID'), nullable=False)
+    UnitID = db.Column(db.Integer, db.ForeignKey('T_UNIT.UnitID'), nullable=False)
+    LocationTypeID = db.Column(db.Integer, db.ForeignKey('T_LOCATION_TYPE.LocationTypeID'), nullable=False)
 
     unit_r = db.relationship('Unit', backref=db.backref('unit_location', lazy=True))
     location_type_r = db.relationship('LocationType', backref=db.backref('location_types', lazy=True))
