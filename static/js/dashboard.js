bcffa1b149afd0418406fe4bdec05599d535cf3a
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-04-06 11:02:01 +0000

    Add functions to ready function

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -107,4 +107,25 @@ $.each($("#LineCharts canvas"), function(key, value) {
     getData($(value).attr("class").replace("Zone", ""), "Zone")
 })
 
-$(getData("novalue", "LineChartData"))
+$(function() {
+    getData("novalue", "LineChartData");
+    $('select').material_select();
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 5, // Creates a dropdown of 15 years to control year,
+        closeOnSelect: false, // Close upon selecting a date,
+        container: undefined, // ex. 'body' will append picker to body
+        today: 'Aujourd\'hui',
+        clear: 'Vider',
+        close: 'Ok',
+        labelMonthNext: 'Mois suivant',
+        labelMonthPrev: 'Mois précedant',
+        labelMonthSelect: 'Selectionner un mois',
+        labelYearSelect: 'Selectionner une année',
+        monthsFull: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        monthsShort: ['Janv.', 'Févr.', 'Mars', 'Avr.', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+        weekdaysFull: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+        weekdaysShort: ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa'],
+        weekdaysLetter: ['D', 'L', 'M', 'M', 'J', 'V', 'S']
+    });
+})
