859bbcf2c619344cff198dfbc9652087d04cbae8
Zakaria AIT AADDI <Z.AITAADDI@copag.local>
2018-04-06 11:06:23 +0000

    Switch order of functions

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,50 +1,38 @@
-function makeLineChart(canvas, ZoneName) {
-    var charts = new Array
-    $.each(canvas, function(key, value) {
-        var ctx = value.getContext('2d')
-        let chart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: [],
-                datasets: []
-            },
-            options: {
-                title: {
-                    display: false,
-                    position: 'top',
-                    text: ZoneName
-                },
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true,
-                            stepSize: 1
-                        }
-                    }],
-                    xAxes: [{
-                        type: 'time',
-                        position: 'bottom',
-                        time: {
-                            minUnit: 'day',
-                            stepSize: 1
-                        },
-                    }]
-                },
-                legend: {
-                    display: true,
-                    position: 'top'
-                },
-                elements: {
-                    line: {
-                        tension: 0.1,
-                    }
-                }
-            }
-        })
-        charts.push(chart)
-    })
-    return charts
+var chartList = new Array
+const colors = {
+    S1: "rgba(218, 58, 47, 1)",
+    S2: "rgba(218, 207, 47, 1)",
+    S3: "rgba(81, 218, 47, 1)",
+    S4: "rgba(47, 130, 218, 1)",
+    S5: "rgba(218, 47, 210, 1)"
 }
+
+$(function() {
+    getData("novalue", "LineChartData");
+    $('select').material_select();
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 5, // Creates a dropdown of 15 years to control year,
+        closeOnSelect: false, // Close upon selecting a date,
+        container: undefined, // ex. 'body' will append picker to body
+        today: 'Aujourd\'hui',
+        clear: 'Vider',
+        close: 'Ok',
+        labelMonthNext: 'Mois suivant',
+        labelMonthPrev: 'Mois précedant',
+        labelMonthSelect: 'Selectionner un mois',
+        labelYearSelect: 'Selectionner une année',
+        monthsFull: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        monthsShort: ['Janv.', 'Févr.', 'Mars', 'Avr.', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+        weekdaysFull: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+        weekdaysShort: ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa'],
+        weekdaysLetter: ['D', 'L', 'M', 'M', 'J', 'V', 'S']
+    });
+    $.each($("#LineCharts canvas"), function(key, value) {
+        getData($(value).attr("class").replace("Zone", ""), "Zone")
+    })
+})
+
 
 function getData(value, field) {
     $.getJSON("/dashboard/api", {
@@ -94,38 +82,51 @@ function getData(value, field) {
     })
 }
 
-var chartList = new Array
-const colors = {
-    S1: "rgba(218, 58, 47, 1)",
-    S2: "rgba(218, 207, 47, 1)",
-    S3: "rgba(81, 218, 47, 1)",
-    S4: "rgba(47, 130, 218, 1)",
-    S5: "rgba(218, 47, 210, 1)"
+
+function makeLineChart(canvas, ZoneName) {
+    var charts = new Array
+    $.each(canvas, function(key, value) {
+        var ctx = value.getContext('2d')
+        let chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: [],
+                datasets: []
+            },
+            options: {
+                title: {
+                    display: false,
+                    position: 'top',
+                    text: ZoneName
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1
+                        }
+                    }],
+                    xAxes: [{
+                        type: 'time',
+                        position: 'bottom',
+                        time: {
+                            minUnit: 'day',
+                            stepSize: 1
+                        },
+                    }]
+                },
+                legend: {
+                    display: true,
+                    position: 'top'
+                },
+                elements: {
+                    line: {
+                        tension: 0.1,
+                    }
+                }
+            }
+        })
+        charts.push(chart)
+    })
+    return charts
 }
-
-$.each($("#LineCharts canvas"), function(key, value) {
-    getData($(value).attr("class").replace("Zone", ""), "Zone")
-})
-
-$(function() {
-    getData("novalue", "LineChartData");
-    $('select').material_select();
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 5, // Creates a dropdown of 15 years to control year,
-        closeOnSelect: false, // Close upon selecting a date,
-        container: undefined, // ex. 'body' will append picker to body
-        today: 'Aujourd\'hui',
-        clear: 'Vider',
-        close: 'Ok',
-        labelMonthNext: 'Mois suivant',
-        labelMonthPrev: 'Mois précedant',
-        labelMonthSelect: 'Selectionner un mois',
-        labelYearSelect: 'Selectionner une année',
-        monthsFull: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
-        monthsShort: ['Janv.', 'Févr.', 'Mars', 'Avr.', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
-        weekdaysFull: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
-        weekdaysShort: ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa'],
-        weekdaysLetter: ['D', 'L', 'M', 'M', 'J', 'V', 'S']
-    });
-})
